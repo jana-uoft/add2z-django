730e440b6b8566f3222553452eb892fc3bca3629
Jana <jana.stc@gmail.com>
2017-01-19 21:59:00 -0500

    Removed Secret KEY and set Debug to read from env var

--- django_project/settings_dev.py
+++ django_project/settings_dev.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ce9dy%b)#u+$dx_7-v8@8iv$fiikln=37bbt6+t552=le*5!pn'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = ['localhost']
 
