5a772a509d1063b8bf386c3fb123ac13aecac03f
Jana <jana.stc@gmail.com>
2017-02-08 20:54:43 -0500

    Update comming soon

--- directory/views.py
+++ directory/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 
+context = {"STATIC_URL" : "https://storage.googleapis.com/add2z/classifieds/"}
+
 # Create your views here.
 def index(request):
-	context = {"STATIC_URL": 'https://storage.googleapis.com/add2z/classifieds/'}
 	return render(request, 'directory/coming_soon.html', context)
